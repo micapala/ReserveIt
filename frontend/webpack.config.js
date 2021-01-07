339f5ab6eeb6e7c200b3ff0823cb256146df73e0
Michał Capała <michaal.capala@gmail.com>
2021-01-07 23:21:27 +0100

    Dodano stronę logowania i współpracę frontendu z backendem

---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".vue"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue?$/,
+        exclude: /(node_modules)/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      "/Users": {
+        target: "http://localhost:5000"
+      }
+    }
+  }
+//   externals: {
+//     // global app config object
+//     config: JSON.stringify({
+//       apiUrl: "http://localhost:8080"
+//     }),
+//   },
+};
